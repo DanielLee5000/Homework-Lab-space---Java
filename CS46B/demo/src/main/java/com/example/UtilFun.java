6fcdefdc19c6887c0b758905a45b566f3cb39406
DanielLee5000 <DanielLee5000>
2026-09-21 11:24:18 -0700

    TestUtilFun.java update

---
+++ CS46B/demo/src/main/java/com/example/UtilFun.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class UtilFun {
+    public UtilFun() {}
+    
+    public int power2(int n) {
+        return 1 << n;
+    }
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    int divide(int a, int b) {
+        return a / b;
+    }
+}
--- CS46B/demo/src/test/java/TestUtilFun.java
+++ CS46B/demo/src/test/java/TestUtilFun.java
@@ -1,6 +1,22 @@
+import com.example.UtilFun;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestUtilFun {
-    
-}
+    public class TestUtilFun {
+        UtilFun utf;
+
+        public TestUtilFun() {
+            utf = new UtilFun();
+        }
+
+        @Test
+        public void testAdd() {
+            assertEquals(5, utf.add(2, 3));
+        }
+
+        @Test
+        public void testPower2() {
+            assertEquals(32, utf.power2(5));
+            assertEquals(512, utf.power2(9));
+        }
+    }
